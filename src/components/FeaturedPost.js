051cc0ab546eb70fbe0843d1b24b56d7ec6bef8c
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2020-06-03 18:10:41 -0500

    corregir fecha_publ en listado de psots

--- src/components/FeaturedPost.js
+++ src/components/FeaturedPost.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Hidden from '@material-ui/core/Hidden';
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Hidden from "@material-ui/core/Hidden";
 
 const useStyles = makeStyles({
   card: {
-    display: 'flex',
+    display: "flex",
   },
   cardDetails: {
     flex: 1,
@@ -35,7 +35,7 @@ export default function FeaturedPost(props) {
                 {post.titulo}
               </Typography>
               <Typography variant="subtitle1" color="textSecondary">
-                {post.fecha_pub}
+                {post.fecha_publ}
               </Typography>
               <Typography variant="subtitle1" paragraph>
                 {post.texto_intro}
@@ -46,7 +46,11 @@ export default function FeaturedPost(props) {
             </CardContent>
           </div>
           <Hidden xsDown>
-            <CardMedia className={classes.cardMedia} image="https://source.unsplash.com/random" title={post.imageTitle} />
+            <CardMedia
+              className={classes.cardMedia}
+              image="https://source.unsplash.com/random"
+              title={post.imageTitle}
+            />
           </Hidden>
         </Card>
       </CardActionArea>
